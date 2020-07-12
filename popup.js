2fbf4b8c73190464d20cb2575bdf2f25dfce2c88
Nicholas Wang <nicholaswang2000@berkeley.edu>
2020-07-12 12:21:49 -0700

    Added spongebobifying logic

--- popup.js
+++ popup.js
@@ -1,4 +1,28 @@
-function myFunction() {
-  var x = document.getElementById("inputArea").nodeValue;
-  console.log(x);
+let translate = document.getElementById("translate");
+
+translate.onclick = function () {
+  var textToTranslate = document.getElementById("inputArea").value;
+  spongebobifyText(textToTranslate);
+};
+
+function spongebobifyText(text) {
+  var spongebobifiedText = "";
+  for (var i = 0; i < text.length; i++) {
+    if (i % 2 == 0) {
+      let upperLowerRand = Math.floor(Math.random() * 3);
+      if (upperLowerRand == 0) {
+        spongebobifiedText += text.charAt(i).toUpperCase();
+      } else {
+        spongebobifiedText += text.charAt(i).toLowerCase();
+      }
+    } else {
+      let upperLowerRand = Math.floor(Math.random() * 3);
+      if (upperLowerRand != 0) {
+        spongebobifiedText += text.charAt(i).toUpperCase();
+      } else {
+        spongebobifiedText += text.charAt(i).toLowerCase();
+      }
+    }
+  }
+  document.getElementById("outputArea").value = spongebobifiedText;
 }
